d3cfd1bfa2bb82a8f6122ab59fb81521ac5f753b
ma3llim007 <mohdsameer68257@gmail.com>
2024-07-10 14:50:25 +0530

    chore: datatypes

---
+++ 00_basic/01_learn_basic/02_datatypes.js/01_datatypes.js
@@ -0,0 +1,34 @@
+// 01. String
+const string = "Mohd Sameer";
+
+// 02. Number
+const Number = 60;
+
+// 03. Bigint
+const Bigint = BigInt(9007199254740991);
+
+// 04. Boolean
+const Boolean = false;
+const Boolean2 = true;
+
+// 05. Undefined
+let Undefined;
+
+// 06. Null
+const Null = null;
+
+// 07. Symbol
+let Sym2 = Symbol("Sameer");
+
+// 08. Object
+const Object = {firstName:"Mohd",lastName:"Sameer"}
+
+console.log(string);
+console.log(Number);
+console.log(Boolean);
+console.log(Boolean2);
+console.log(Undefined);
+console.log(Null);
+console.log(Sym2);
+console.log(Object);
+console.log(typeof(Object));
