ed3b44751843fdf17b9f96bc8003cbbcb4c10af0
ma3llim007 <mohdsameer68257@gmail.com>
2025-07-08 20:43:47 +0530

    chore: add second approach of solving with recurive

--- 07_math_algorithm/02_factorial_of_a_number.js
+++ 07_math_algorithm/02_factorial_of_a_number.js
@@ -1,3 +1,4 @@
+// 1. First Approach
 const factorial = (n) => {
     let result = 1;
     for (let i = 2; i <= n; i++) {
@@ -6,10 +7,16 @@ const factorial = (n) => {
     return result;
 };
 
-// console.log(factorial(0));
-// console.log(factorial(1));
-// console.log(factorial(3));
-// console.log(factorial(5));
+// 2. Second Approach
+const factorialWithRecursive = (n) => {
+    if (n <= 1) return 1;
+    return n * factorialWithRecursive(n - 1);
+};
 
+console.log(factorialWithRecursive(0));
+console.log(factorialWithRecursive(1));
+console.log(factorialWithRecursive(3));
+console.log(factorialWithRecursive(4));
+console.log(factorialWithRecursive(5));
 
 // Big-0 = O(n)
