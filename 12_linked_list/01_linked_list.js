feb9bd6b13cd7203a5ddad0f6491243433658507
ma3llim007 <mohdsameer68257@gmail.com>
2025-08-03 18:39:58 +0530

    chore: start learning the linked list

---
+++ 12_linked_list/01_linked_list.js
@@ -0,0 +1,88 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    getSize() {
+        return this.size;
+    }
+
+    // O(1)
+    prepend(value) {
+        const node = new Node(value);
+        if (this.isEmpty()) {
+            this.head = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
+    }
+
+    // O(n)
+    append(value) {
+        const node = new Node(value);
+        if (this.isEmpty()) {
+            this.head = node;
+        } else {
+            let prev = this.head;
+            while (prev.next) {
+                prev = prev.next;
+            }
+            prev.next = node;
+        }
+        this.size++;
+    }
+
+    insert(value, index) {
+        if (index < 0 || index > this.size) {
+            return;
+        }
+        if (index === 0) {
+            this.prepend(value);
+        } else {
+            const node = new Node(value);
+            let prev = this.head;
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            node.next = prev.next;
+            prev.next = node;
+            this.size++;
+        }
+    }
+    print() {
+        if (this.isEmpty()) {
+            console.log("List is Empty");
+        } else {
+            let current = this.head;
+            let listValue = [];
+            while (current) {
+                listValue.push(current.value);
+                current = current.next;
+            }
+            console.log(listValue.join(" "));
+        }
+    }
+}
+
+const list = new LinkedList();
+// console.log("List is Empty? ", list.isEmpty());
+// console.log("List Size", list.getSize());
+list.insert(10, 0);
+list.insert(20, 1);
+list.insert(30, 2);
+
+list.print();
