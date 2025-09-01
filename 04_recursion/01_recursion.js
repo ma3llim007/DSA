743d41aa05250b652a84ac902163ca353e224a00
ma3llim007 <mohdsameer68257@gmail.com>
2025-09-01 22:37:31 +0530

    chore: learn few new prolem with different approach

--- 04_recursion/01_recursion.js
+++ 04_recursion/01_recursion.js
@@ -5,4 +5,45 @@ function Func() {
     count = count + 1;
     Func();
 }
-Func();
+
+const numberPrint = (n) => {
+    if (n == 0) return;
+    numberPrint(n - 1);
+    console.log(n);
+};
+
+// Sum of number
+const sumOfNumber = (n) => {
+    if (n == 0) return 0;
+    return n + sumOfNumber(n - 1);
+};
+
+// factorial of number
+const factorialOfNumber = (n) => {
+    if (n == 0) return 1;
+    return n * factorialOfNumber(n - 1);
+};
+
+// fibonacci_number
+const fibonacci_number = (n) => {
+    let first = 0,
+        second = 1;
+    process.stdout.write(first + " " + second + " ");
+
+    for (let i = 1; i <= n - 2; i++) {
+        let third = first + second;
+        first = second;
+        second = third;
+        process.stdout.write(third + " ");
+    }
+};
+
+function fiboNt(n, first, second) {
+    if (n == 0) return;
+    let third = first + second;
+    process.stdout.write(third + " ");
+    fiboNt(n - 1, second, third);
+}
+process.stdout.write(0 + " " + 1 + " ");
+
+fiboNt(10, 0, 1);
