b9eb4dfa369ea577af7624822695c326243a59e1
ma3llim007 <mohdsameer68257@gmail.com>
2025-09-17 20:24:54 +0530

    chore: solved reverser words in string

--- 03_string/05_reverse_words_in_a_string.js
+++ 03_string/05_reverse_words_in_a_string.js
@@ -1,12 +1,22 @@
-const reverseWords = (string) => {
-    let stringToArray = string.split(" ");
-    console.log(stringToArray.length);
-    
-    for (let i = stringToArray.length - 1; i < 0; i--) {
-        console.log(stringToArray[i]);
+// Brute Force Approach
+const reverseWords = (s) => {
+    let words = s.split(" ");
+    let result = [];
+    for (let i = words.length - 1; i >= 0; i--) {
+        if (words[i]) result.push(words[i]);
     }
+
+    return result.join(" ");
+};
+
+// Better Approach
+const reverseWordsBetter = (s) => {
+    return s.split(" ").filter((s) => s.length > 0).reverse().join(" ");
 };
 let example1 = "this is an amazing program";
 let example2 = "This is decent";
+let example3 = "the sky is blue  ";
 
-reverseWords(example1);
+console.log(reverseWordsBetter(example1));
+console.log(reverseWordsBetter(example2));
+console.log(reverseWordsBetter(example3));
