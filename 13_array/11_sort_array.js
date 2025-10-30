bc69803bda0e2bca5b74ae4573a392cd30e98b1b
ma3llim007 <mohdsameer68257@gmail.com>
2025-10-30 20:58:21 +0530

    chore: solve sort array problem with better approach

--- 13_array/11_sort_array.js
+++ 13_array/11_sort_array.js
@@ -1,5 +1,25 @@
-const sortColors = (nums) => {
+const sortColorsBruteForce = (nums) => {
     return nums.sort((a, b) => a - b);
+};
+
+const sortColors = (nums) => {
+    let count0 = 0,
+        count1 = 0,
+        count2 = 0;
+
+    for (let num of nums) {
+        if (num === 0) count0++;
+        else if (num === 1) count1++;
+        else if (num === 2) count2++;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i < count0) nums[i] = 0;
+        else if (i < count0 + count1) nums[i] = 1;
+        else nums[i] = 2;
+    }
+
+    return nums
 };
 
 console.log(sortColors([2, 0, 2, 1, 1, 0]));
